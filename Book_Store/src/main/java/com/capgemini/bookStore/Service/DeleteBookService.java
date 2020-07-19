9bf0321a40e75709a66bed91340ee52c5f5bd818
aishu-22 <51692680+aishu-22@users.noreply.github.com>
2020-07-19 11:35:39 +0530

    Delete DeleteBookService.java

--- Book_Store/src/main/java/com/capgemini/bookStore/Service/DeleteBookService.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.bookStore.Service;
-
-import com.capgemini.bookStore.Exceptions.DeleteBookException;
-
-public interface DeleteBookService {
-
-	public String deleteBook(int bookId) throws DeleteBookException;
-	}
-
